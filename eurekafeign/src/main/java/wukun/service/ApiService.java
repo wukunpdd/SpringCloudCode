6b934f800696d72638445b2ab89fa446958496dd
wukunpdd <wukunpdd@163.com>
2018-11-02 16:09:41 +0800

    服务异常处理

--- eurekafeign/src/main/java/wukun/service/ApiService.java
+++ eurekafeign/src/main/java/wukun/service/ApiService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "eurekaclient")
+@FeignClient(value = "eurekaclient", fallback = ApiServiceError.class)
 public interface ApiService {
 
     @RequestMapping(value = "/index",method = RequestMethod.GET)
