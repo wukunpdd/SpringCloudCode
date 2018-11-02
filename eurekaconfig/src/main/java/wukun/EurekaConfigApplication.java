3ab4f7fa3c51a93c0dcf9525efa801b926d509f8
wukunpdd <wukunpdd@163.com>
2018-11-02 16:33:56 +0800

    配置中心--建立

---
+++ eurekaconfig/src/main/java/wukun/EurekaConfigApplication.java
@@ -0,0 +1,16 @@
+package wukun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class EurekaConfigApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(EurekaConfigApplication.class, args);
+    }
+}
